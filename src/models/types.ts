0a9f6d4959268aca1e57c5ccd28f4cfab26cfe9e
DmitrySavritsky <dmitry.savritsky@yandex.ru>
2021-02-10 14:19:21 +0300

    Added types for schemas

---
+++ src/models/types.ts
@@ -0,0 +1,58 @@
+import { Types } from "mongoose";
+
+export interface IPost {
+  ownerId: Types.ObjectId;
+  message: String;
+  date: Date;
+  comments: Array<Types.ObjectId>;
+  likes: Array<Types.ObjectId>;
+}
+
+export interface IUser {
+  login: String;
+  password: String;
+  firstName: String;
+  lastName: String;
+  email: String;
+  posts: Array<Types.ObjectId>;
+  friends: Array<Types.ObjectId>;
+  friendRequests: Array<Types.ObjectId>;
+}
+
+export interface IMessage {
+  ownerId: Types.ObjectId;
+  chatId: Types.ObjectId;
+  message: String;
+  date: Date;
+}
+
+export interface IChat {
+  chatName: String;
+  messages: Array<Types.ObjectId>;
+  users: Array<Types.ObjectId>;
+}
+
+export interface IComment {
+  postId: Types.ObjectId;
+  ownerId: Types.ObjectId;
+  message: String;
+  date: Date;
+}
+
+export interface LoginData {
+  login: string;
+  password: string;
+}
+
+export interface PostData {
+  message: string;
+}
+
+export interface LikeData {
+  postId: Types.ObjectId;
+}
+
+export interface TokenInterface {
+  id: Types.ObjectId;
+  login: string;
+}
