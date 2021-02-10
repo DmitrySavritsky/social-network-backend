c91c9b9d19f494278d6d091cf52f5395e68c0ccb
DmitrySavritsky <dmitry.savritsky@yandex.ru>
2021-02-10 14:18:39 +0300

    Added authentification middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,39 @@
+import jwt from "jsonwebtoken";
+import { IUser, TokenInterface } from "../models/types";
+import UserSchema from "../models/user.model";
+import { Request, Response, NextFunction } from "express";
+
+async function isUserExists(login: string, request: Request) {
+  const user = (await UserSchema.findOne({ login: login })) as IUser;
+  if (user !== null) {
+    request.currentUser = user;
+    return true;
+  } else return false;
+}
+
+const auth = async function (req: Request, res: Response, next: NextFunction) {
+  try {
+    if (req?.headers?.authorization !== undefined) {
+      const [strategy, token] = req.headers.authorization.split(" ");
+      const result = jwt.verify(token, "veryStrongSecretKey");
+      const login = (result as TokenInterface).login;
+      req.login = login;
+      req.id = (result as TokenInterface).id;
+
+      const isExists = await isUserExists(login, req);
+      if (!isExists) {
+        console.log("No user!");
+        throw new Error("User not exists!");
+      }
+    } else {
+      console.log("no header");
+      throw new Error("No authorization header exists!");
+    }
+    next();
+  } catch (err) {
+    console.log()
+    res.status(403).send(err.message);
+  }
+};
+
+export default auth;
